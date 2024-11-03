676b69c63c235b3d20b1768cd0d3514bc63b040a
nikeshdc-25 <nutternikexdx@gmail.com>
2024-11-03 22:18:21 +0545

    final secret internal fix 1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,5 +9,22 @@ export default defineConfig({
     },
   },
   plugins: [
-    react()]
+    react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+    minify: "esbuild", // Use esbuild for minification
+    sourcemap: true, // Helpful for debugging and analysis
+  },
 });

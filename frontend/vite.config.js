4366b0f541bc5b29810f87098f7199737750b2bd
nikeshdc-25 <nutternikexdx@gmail.com>
2024-11-03 19:08:11 +0545

    rm rollup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer"; // For analyzing bundle size
 
 export default defineConfig({
   server: {
@@ -10,28 +9,5 @@ export default defineConfig({
     },
   },
   plugins: [
-    react(),
-    visualizer({
-      open: true, // Opens the stats report in the browser
-      filename: "bundle-visualizer.html",
-      template: "treemap", // Options: sunburst, treemap, network, circle
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
-    minify: "esbuild", // Use esbuild for minification
-    sourcemap: true, // Helpful for debugging and analysis
-  },
+    react()],
 });

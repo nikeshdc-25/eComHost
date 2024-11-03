a078960f7b12d19213cb11b3145dc0cd4ce8dd3c
nikeshdc-25 <nutternikexdx@gmail.com>
2024-11-03 19:12:42 +0545

    vite install

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer"; // For analyzing bundle size
 
 export default defineConfig({
   server: {
@@ -9,5 +10,28 @@ export default defineConfig({
     },
   },
   plugins: [
-    react()],
+    react(),
+    visualizer({
+      open: true, // Opens the stats report in the browser
+      filename: "bundle-visualizer.html",
+      template: "treemap", // Options: sunburst, treemap, network, circle
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+    minify: "esbuild", // Use esbuild for minification
+    sourcemap: true, // Helpful for debugging and analysis
+  },
 });

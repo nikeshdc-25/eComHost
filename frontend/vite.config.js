a800b06a8a7cf1c9ca42152031f3cda57964d526
nikeshdc-25 <nutternikexdx@gmail.com>
2024-11-03 18:48:33 +0545

    latest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,5 +9,29 @@ export default defineConfig({
       "/uploads": "http://localhost:3000",
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true, // Opens the stats report in the browser
+      filename: "bundle-visualizer.html",
+      template: "treemap", // Options: sunburst, treemap, network, circle
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+    minify: "esbuild", // Use esbuild for minification
+    sourcemap: true, // Helpful for debugging and analysis
+  },
 });

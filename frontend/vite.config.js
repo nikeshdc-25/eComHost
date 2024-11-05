0f842a2d09bd90fb74ed98e2a9f44ce475d2666e
nikeshdc-25 <nutternikexdx@gmail.com>
2024-11-05 09:49:29 +0545

    vite change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -8,21 +9,5 @@ export default defineConfig({
       "/uploads": "http://localhost:3000",
     },
   },
-  plugins: [
-    react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
-  },
+  plugins: [react()],
 });

672fa02ad78f01fc5c2d16a8cab32fc85b3e4ecf
nikeshdc-25 <nutternikexdx@gmail.com>
2024-11-16 16:34:55 +0545

    product page

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -57,6 +57,10 @@ const productSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    totalSold: {
+      type: Number,
+      default: 0,
+    },
     reviews: [reviewSchema],
     properties: [
       {
